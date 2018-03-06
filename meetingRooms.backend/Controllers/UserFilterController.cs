9639dadb5a2587626b6a492cf1f5edc2947f86c5
Denis <osipppp@gmail.com>
2018-03-07 03:03:55 +0700

    filter: Surname+email P.S. Postman has error

---
+++ meetingRooms.backend/Controllers/UserFilterController.cs
@@ -0,0 +1,21 @@
+﻿using meetingRooms.backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace meetingRooms.backend.Controllers
+{
+    public class UserFilterController : ApiController
+    {
+        
+        [Route("users/{userName}")]
+        public IEnumerable<User> GetUsers(string userName)
+        {
+            return UsersDataSource.All.OfType<User>().Where(i => i.Surname.StartsWith(userName)|i.Email.StartsWith(userName));
+        }
+    }
+}
---
+++ meetingRooms.backend/Models/UsersDataSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace meetingRooms.backend.Models
+{
+    public class UsersDataSource
+    {
+        private static List<User> _users = null;
+        public static List<User> All
+        {
+            get
+            {
+                if (_users == null)
+                {
+                    _users = new List<User>();
+                    _users.Add(new User() { Id=1, Surname="ivanov", Email="ivanov@sibedge.com", Url="qweqwe"});
+                    _users.Add(new User() { Id = 2, Surname = "ivanenko", Email = "sidorov@sibedge.com", Url = "qweqwe" });
+                    _users.Add(new User() { Id = 3, Surname = "ivashuk", Email = "petrov@sibedge.com", Url = "qweqwe" });
+                    _users.Add(new User() { Id = 4, Surname = "сааамсонов", Email = "ger@sibedge.com", Url = "qweqwe" });
+                    _users.Add(new User() { Id = 5, Surname = "сааааников", Email = "qweqw@sibedge.com", Url = "qweqwe" });
+                    _users.Add(new User() { Id = 5, Surname = "саников", Email = "asd@sibedge.com", Url = "qweqwe" });
+                }
+                return _users;
+            }
+        }
+    }
+}
