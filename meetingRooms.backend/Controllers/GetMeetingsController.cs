6dafba398e5916019d827c1919912f905b552608
Alexander Ivanov <ivanov4291@gmail.com>
2018-03-06 22:19:58 +0700

    Добавьте файлы проекта.

---
+++ meetingRooms.backend/Controllers/GetMeetingsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace meetingRooms.backend.Controllers
+{
+    public class GetMeetingsController : ApiController
+    {
+        [Route("meetings")]
+        public IHttpActionResult GetMeetings()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
---
+++ meetingRooms.backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="meetingRooms.backend.WebApiApplication" Language="C#" %>
