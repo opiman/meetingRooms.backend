47b7f726bc95d1c31c4d657cd4c249b1f4b8ef80
Alexander Ivanov <ivanov4291@gmail.com>
2018-03-07 13:49:51 +0700

    Get today events by meeting rooms

--- meetingRooms.backend/Controllers/GetMeetingsController.cs
+++ meetingRooms.backend/Controllers/GetMeetingsController.cs
@@ -4,15 +4,51 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.Exchange.WebServices.Data;
+using meetingRooms.backend.Models;
 
 namespace meetingRooms.backend.Controllers
 {
     public class GetMeetingsController : ApiController
     {
-        [Route("meetings")]
-        public IHttpActionResult GetMeetings()
+        private MeetingRoom[] rooms = new MeetingRoom[]
         {
-            return Ok("Hello world");
+            new MeetingRoom { Email = "large-meeting-room@sibedge.com", Name = "Большая переговорка"},
+            new MeetingRoom { Email = "small-meeting-room@sibedge.com", Name = "Малая переговорка"}
+        };
+
+        [Route("meetings/{id}")]
+        public IEnumerable<Meeting> GetMeetings(int id)
+        {
+            ExchangeService service = new ExchangeService(); //Auth
+            service.Credentials = new WebCredentials(PrivateVariables.Email, PrivateVariables.Passwrord);
+            service.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+
+            FolderId folderIdFromCalendar;
+            try
+            {
+                folderIdFromCalendar = new FolderId(WellKnownFolderName.Calendar, rooms[id].Email); //Get room calendar folder
+            } catch(Exception)
+            {
+                throw new System.Web.HttpException(400, "Incorrect room id");
+            }
+            
+
+            DateTime startDate = DateTime.Today;
+            CalendarView cView = new CalendarView(startDate, startDate.AddDays(1), 200); //Set date
+
+            FindItemsResults<Item> appointments = service.FindItems(folderIdFromCalendar, cView); //Get Appointments by room
+            List<Meeting> meetings = new List<Meeting>();
+            foreach(Appointment appointment in appointments)
+                meetings.Add(new Meeting()
+                    {
+                        Subject = appointment.Subject,
+                        StartTime = appointment.Start,
+                        Duration = appointment.Duration
+                    }
+                );
+
+            return meetings;
         }
     }
 }
